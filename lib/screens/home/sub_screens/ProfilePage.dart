0386253cda80f1af0345255e05bc428ed4a70690
Jaydip Pawar <62420406+jaydip-pawar@users.noreply.github.com>
2022-03-14 22:26:15 +0530

    Improved Profile Page

--- lib/screens/home/sub_screens/ProfilePage.dart
+++ lib/screens/home/sub_screens/ProfilePage.dart
@@ -8,156 +8,111 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Column(
         children: [
-          Column(
+          const Text(
+            "George Thomas",
+            style: TextStyle(fontSize: 20, color: Colors.black87),
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Text(
-                "George Thomas",
-                style: TextStyle(fontSize: 20, color: Colors.black87),
+            children: [
+              Column(
+                children: const [
+                  Text(
+                    '168 ',
+                    style: TextStyle(fontSize: 20, color: Colors.black87),
+                  ),
+                  Text(
+                    'Problems Posted',
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: MediaQuery.of(context).size.width / 4,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 5),
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/images/cool.jpg'),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 1,
+                    right: 1,
+                    child: CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Colors.black87,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                          size: 13,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: const [
+                  Text(
+                    '40 ',
+                    style: TextStyle(fontSize: 20, color: Colors.black87),
+                  ),
+                  Text(
+                    'Problems Resolved',
+                  ),
+                ],
               ),
             ],
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerLeft,
-                child:
-                Text(
-                  '168 ',
-                  style: TextStyle(fontSize: 20, color: Colors.black87),
-
-                ),
-              ),
-            ],
-
+          const Text(
+            'LEVEL 5',
+            style: TextStyle(fontSize: 20, color: Colors.black87),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerLeft,
-                child:
-                Text(
-                  'Problems Posted',
-                ),
-              ),
-            ],
-
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child:
-                Text(
-                  '40 ',
-                  style: TextStyle(fontSize: 20, color: Colors.black87),
-
-                ),
-              ),
-            ],
-
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child:
-                Text(
-                  'Problems Resolved',
-                ),
-              ),
-            ],
-
-          ),
-
           Container(
-            padding: const EdgeInsets.all(10.0),
-            width: MediaQuery.of(context).size.width / 4,
-            height: MediaQuery.of(context).size.width / 4,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white, width: 5),
-              shape: BoxShape.circle,
-              color: Colors.white,
-              image: const DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/images/cool.jpg'),
+            padding: const EdgeInsets.all(20),
+            child: LinearPercentIndicator(
+              width: 180.0,
+              //lineWidth: 15.0,
+              lineHeight: 15,
+              percent: 50 / 100,
+              animation: true,
+              animationDuration: 2000,
+              leading: const Text(
+                "50%",
+                style: TextStyle(fontSize: 20),
               ),
+              trailing: const Text(
+                "150/300",
+                style: TextStyle(fontSize: 20, color: Colors.blueGrey),
+              ),
+              progressColor: Colors.blueGrey,
             ),
           ),
-
-
-
-          Padding(
-            padding: const EdgeInsets.only(),
-            child: CircleAvatar(
-
-              backgroundColor: Colors.black54,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.edit,
-                  color: Colors.white,
-                ),
-                onPressed: () {},
+          Flexible(
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.0,
+                mainAxisSpacing: 1.0,
               ),
+              children: [
+                Gallery(),
+              ],
             ),
           ),
-
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.center,
-                child:
-                Text(
-                  'LEVEL 5',
-                  style: TextStyle(fontSize: 20, color: Colors.black87),
-                ),
-              ),
-            ],
-
-          ),
-
-          Container(
-              padding: const EdgeInsets.all(20),
-              child: LinearPercentIndicator(
-                width: 180.0,
-                //lineWidth: 15.0,
-                lineHeight: 15,
-                percent: 50 / 100,
-                animation: true,
-                animationDuration: 2000,
-                leading: const Text(
-                  "50%",
-                  style: TextStyle(fontSize: 20),
-                ),
-                trailing: const Text(
-                  "150/300",
-                  style: TextStyle(fontSize: 20, color: Colors.blueGrey),
-                ),
-                progressColor: Colors.blueGrey,
-              )),
-
-      Expanded(
-        child: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 1.0,
-            mainAxisSpacing: 1.0,
-
-          ),
-          children: [
-            Gallery(),
-          ],
-        ),
+        ],
       ),
-  ]),
     );
   }
 }
